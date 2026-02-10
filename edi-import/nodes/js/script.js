e951f5c558eb9cfbec1e15a493899ba7ff484779
Huff <jeremythuff@gmail.com>
2026-02-10 14:31:14 -0600

    EDI Import - Added extract okapi token

--- edi-import/nodes/js/script.js
+++
@@ -1,9 +0,0 @@
-
-let exampleValue = "original value";
-print("\n\nOriginal value is: " + exampleValue + "\n");
-
-if ("{{exampleInjectValue}}" != "") {
-  exampleValue = "{{exampleInjectValue}}";
-}
-
-print("\nResulting value is: " + exampleValue + "\n\n");
