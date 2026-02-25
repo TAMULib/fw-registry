b0e80da3f0b3cf13ecb1f11d21452edc994f51eb
Huff <jeremythuff@gmail.com>
2026-02-25 14:26:14 -0600

    EDI Import: added parsing script

--- edi-import/nodes/js/subprocessLog.js
+++ edi-import/nodes/js/subprocessLog.js
@@ -1,3 +1,4 @@
  print('\n\n------------\n');
  const invoice = JSON.parse(inv);
- print('tag = ' + invoice.name + '\n');
+ print('tag = ' + invoice.account_number + '\n');
+ print('tag = ' + invoice.edi + '\n');
