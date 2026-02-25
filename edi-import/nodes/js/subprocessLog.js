7e7659cd83e05a3fc3db466034549ce76d48ecbc
Huff <jeremythuff@gmail.com>
2026-02-25 14:39:26 -0600

    EDI Import: now reading edi file from disk

--- edi-import/nodes/js/subprocessLog.js
+++ edi-import/nodes/js/subprocessLog.js
@@ -1,4 +1,4 @@
  print('\n\n------------\n');
  const invoice = JSON.parse(inv);
- print('tag = ' + invoice.account_number + '\n');
- print('tag = ' + invoice.edi + '\n');
+ print('account number = ' + invoice.account_number + '\n');
+ print('edi = ' + invoice.edi + '\n');
