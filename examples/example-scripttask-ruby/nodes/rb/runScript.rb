7bcb9b0109aa906a360eb997eaeb529150ab7de9
Kevin Day <kday@tamu.edu>
2026-08-13 16:58:45 -0500

    Issue 504: Tweak the example scripts.
    Change the logging a little bit.

--- examples/example-scripttask-ruby/nodes/rb/runScript.rb
+++ examples/example-scripttask-ruby/nodes/rb/runScript.rb
@@ -3,7 +3,10 @@ print "\nHello World from Ruby workflow!!\n\n"
 print "Environment Variables:\n";
 print "======================\n";
 print "- logLevel: " + (execution.hasVariable("logLevel") ? execution.getVariable("logLevel") : "") + "\n";
-print "- folioTenant: " + (execution.hasVariable("folioTenant") ? execution.getVariable("folioTenant") : "") + "\n";
+print "- folioUser: " + (execution.hasVariable("folioUser") ? execution.getVariable("folioUser") : "") + "\n";
 print "- folioPass: " + (execution.hasVariable("folioPass") ? execution.getVariable("folioPass") : "") + "\n";
-print "- tenant: " + (execution.hasVariable("tenant") ? execution.getVariable("tenant") : "") + "\n";
+print "- folioTenant: " + (execution.hasVariable("folioTenant") ? execution.getVariable("folioTenant") : "") + "\n";
+print "- gatewayUrl: " + (execution.hasVariable("gatewayUrl") ? execution.getVariable("gatewayUrl") : "") + "\n";
+print "- gatewayLoginPath: " + (execution.hasVariable("gatewayLoginPath") ? execution.getVariable("gatewayLoginPath") : "") + "\n";
+print "- token: " + (execution.hasVariable("token") ? execution.getVariable("token") : "") + "\n";
 print "- hello_world: " + (execution.hasVariable("hello_world") ? execution.getVariable("hello_world") : "") + "\n\n";
