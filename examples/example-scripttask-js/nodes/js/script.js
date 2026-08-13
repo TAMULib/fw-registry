033251f0009d8f35995fb89a36a7950238db57a0
Kevin Day <kday@tamu.edu>
2026-08-13 15:24:53 -0500

    Issue 504: Add additional logging in scripts to see new variables.
    Provide additional information that shows how new variables can be accessed in the example workflows.

--- examples/example-scripttask-js/nodes/js/script.js
+++ examples/example-scripttask-js/nodes/js/script.js
@@ -7,3 +7,11 @@ if ("{{exampleInjectValue}}" != "") {
 }
 
 print("\nResulting value is: " + exampleValue + "\n\n");
+
+print("Environment Variables:");
+print("======================");
+print("- logLevel: " + execution.getVariable("logLevel"));
+print("- folioTenant: " + execution.getVariable("folioTenant"));
+print("- folioPass: " + execution.getVariable("folioPass"));
+print("- tenant: " + execution.getVariable("folioTenant"));
+print("- hello_world: " + execution.getVariable("helloWorld") + "\n");
--- examples/example-scripttask-ruby/nodes/rb/runScript.rb
+++ examples/example-scripttask-ruby/nodes/rb/runScript.rb
@@ -1,2 +1,9 @@
-print "\nHello World from Ruby workflow!!\n"
+print "\nHello World from Ruby workflow!!\n\n"
 
+print "Environment Variables:\n";
+print "======================\n";
+print "- logLevel: " + (execution.hasVariable("logLevel") ? execution.getVariable("logLevel") : "") + "\n";
+print "- folioTenant: " + (execution.hasVariable("folioTenant") ? execution.getVariable("folioTenant") : "") + "\n";
+print "- folioPass: " + (execution.hasVariable("folioPass") ? execution.getVariable("folioPass") : "") + "\n";
+print "- tenant: " + (execution.hasVariable("tenant") ? execution.getVariable("tenant") : "") + "\n";
+print "- hello_world: " + (execution.hasVariable("hello_world") ? execution.getVariable("hello_world") : "") + "\n\n";
