439ba3c539a8774be85bf5df687b8c3f6ac5bf5a
Kevin Day <kday@tamu.edu>
2026-08-14 11:14:36 -0500

    Issue 504: Improve example ruby script handling.
    The `hasVariable()` can return `true` if the variable is defined but assigned **NULL**.
    This means that the **NULL** issue still exists with the **Ruby** code.
    
    Refactor the logic to use logical **OR** operator and fall back to a string when `getVariable()` returns **NULL**.

--- examples/example-scripttask-ruby/nodes/rb/runScript.rb
+++ examples/example-scripttask-ruby/nodes/rb/runScript.rb
@@ -1,12 +1,13 @@
 print "\nHello World from Ruby workflow!!\n\n"
+;
 
 print "Environment Variables:\n";
 print "======================\n";
-print "- logLevel: " + (execution.hasVariable("logLevel") ? execution.getVariable("logLevel") : "") + "\n";
-print "- folioUser: " + (execution.hasVariable("folioUser") ? execution.getVariable("folioUser") : "") + "\n";
-print "- folioPass: " + (execution.hasVariable("folioPass") ? execution.getVariable("folioPass") : "") + "\n";
-print "- folioTenant: " + (execution.hasVariable("folioTenant") ? execution.getVariable("folioTenant") : "") + "\n";
-print "- folioToken: " + (execution.hasVariable("folioToken") ? execution.getVariable("folioToken") : "") + "\n";
-print "- gatewayUrl: " + (execution.hasVariable("gatewayUrl") ? execution.getVariable("gatewayUrl") : "") + "\n";
-print "- gatewayLoginPath: " + (execution.hasVariable("gatewayLoginPath") ? execution.getVariable("gatewayLoginPath") : "") + "\n";
-print "- hello_world: " + (execution.hasVariable("hello_world") ? execution.getVariable("hello_world") : "") + "\n\n";
+print "- logLevel: " + (execution.getVariable("logLevel") || "") + "\n";
+print "- folioUser: " + (execution.getVariable("folioUser") || "") + "\n";
+print "- folioPass: " + (execution.getVariable("folioPass") || "") + "\n";
+print "- folioTenant: " + (execution.getVariable("folioTenant") || "") + "\n";
+print "- folioToken: " + (execution.getVariable("folioToken") || "") + "\n";
+print "- gatewayUrl: " + (execution.getVariable("gatewayUrl") || "") + "\n";
+print "- gatewayLoginPath: " + (execution.getVariable("gatewayLoginPath") || "") + "\n";
+print "- hello_world: " + (execution.getVariable("hello_world") || "") + "\n\n";
