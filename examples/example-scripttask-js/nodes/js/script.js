12c7bf2c5e21a5109cb691ec3bffb05bf7071a81
Kevin Day <kday@tamu.edu>
2026-08-14 09:59:55 -0500

    Issue 504: Use folioToken instead of token.

--- examples/example-scripttask-js/nodes/js/script.js
+++ examples/example-scripttask-js/nodes/js/script.js
@@ -14,7 +14,7 @@ print("- logLevel: " + execution.getVariable("logLevel"));
 print("- folioUser: " + execution.getVariable("folioUser"));
 print("- folioPass: " + execution.getVariable("folioPass"));
 print("- folioTenant: " + execution.getVariable("folioTenant"));
+print("- folioToken: " + execution.getVariable("folioToken"));
 print("- gatewayUrl: " + execution.getVariable("gatewayUrl"));
 print("- gatewayLoginPath: " + execution.getVariable("gatewayLoginPath"));
-print("- token: " + execution.getVariable("token"));
 print("- hello_world: " + execution.getVariable("helloWorld") + "\n");
